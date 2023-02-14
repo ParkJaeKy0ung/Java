cc599e5dbf258f5f8ccbb1160d4270ce6a11fc8b
JaeKyoung <worudeee@naver.com>
2023-02-14 17:51:32 +0900

    20230214_ 객체지향언어

--- 01_Java/a004_array/src/edu/kh/array/ex/ArrayRun2.java
+++ 01_Java/a004_array/src/edu/kh/array/ex/ArrayRun2.java
@@ -5,9 +5,9 @@ public class ArrayRun2 {
 		
 		ArrayEx2 ae2 = new ArrayEx2();
 		
-		 ae2.ex1();
+		// ae2.ex1();
 		// ae2.ex2();
-		// ae2.ex3();
+		 ae2.ex3();
 		// ae2.ex4();
 		// ae2.ex5();
 
