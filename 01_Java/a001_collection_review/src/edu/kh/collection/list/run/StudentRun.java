93d45dcc0ceb6f861e55341df8cf8b1801a0ab97
JaeKyoung <worudeee@naver.com>
2023-03-03 17:46:10 +0900

    20230303_복습

--- 01_Java/a001_collection_review/src/edu/kh/collection/list/run/StudentRun.java
+++ 01_Java/a001_collection_review/src/edu/kh/collection/list/run/StudentRun.java
@@ -1,9 +1,13 @@
 package edu.kh.collection.list.run;
 
+import edu.kh.collection.list.view.StudentView;
+
 public class StudentRun {
 
-	public StudentRun() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		
+		StudentView view = new StudentView();
+		view.displayMenu();
 	}
 
 }
--- 01_Java/a001_collection_review/src/edu/kh/collection/list/service/StudentService.java
+++ 01_Java/a001_collection_review/src/edu/kh/collection/list/service/StudentService.java
@@ -1,13 +1,30 @@
 package edu.kh.collection.list.service;
 
-import edu.kh.collection.list.view.StudentView;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.kh.collection.list.dto.Student;
 
 public class StudentService {
-
-	public static void main(String[] args) {
-		
-		StudentView view = new StudentView();
-		view.displayMenu();
+	
+	private List<Student> studentList = new ArrayList<Student>();
+	
+	
+	/** studentList에 학생 추가
+	 * @param std
+	 * @return true
+	 */
+	public boolean addStudent(Student std) {
+		return studentList.add(std);
 	}
+
+	
+	/** 학생 전체 조회 서비스
+	 * @return studentList
+	 */
+	public List<Student> selectAll() {
+		return studentList;
+	}
+
 
 }
