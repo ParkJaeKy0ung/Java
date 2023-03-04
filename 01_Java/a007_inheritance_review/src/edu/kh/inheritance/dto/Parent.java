0c0425d6b10846b7a66f285afece34500731cf97
JaeKyoung <worudeee@naver.com>
2023-03-04 23:01:44 +0900

    20230304_복습

--- 01_Java/a007_inheritance_review/src/edu/kh/inheritance/dto/Parent.java
+++ 01_Java/a007_inheritance_review/src/edu/kh/inheritance/dto/Parent.java
@@ -1,5 +1,41 @@
 package edu.kh.inheritance.dto;
 
 public class Parent {
+	
+	private int money = 400_000_000;
+	private String lastName = "박";
+	
+	public Parent() {
+		System.out.println("parent() 기본 생성자");
+	}
 
+	public Parent(int money, String lastName) {
+		super();
+		this.money = money;
+		this.lastName = lastName;
+		System.out.println("parent(int, String) 매개변수 생성자");
+	}
+
+	
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Parent [money=" + money + ", lastName=" + lastName + "]";
+	}
 }
--- 01_Java/a007_inheritance_review/src/edu/kh/inheritance/run/TestRun.java
+++ 01_Java/a007_inheritance_review/src/edu/kh/inheritance/run/TestRun.java
@@ -1,5 +1,13 @@
 package edu.kh.inheritance.run;
 
 public class TestRun {
+	public static void main(String[] args) {
+		
+		Parent p = new Parent(1_000_000_000, "김");
+		
+		
+		
+		
+	}
 
 }
