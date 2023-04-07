33ba66ef132a8da4264b639bc7e33bb742b13cc2
JaeKyoung <worudeee@naver.com>
2023-04-07 17:47:48 +0900

    20230407_programmers

--- Programmers/a00/src/a00/Minus.java
+++
@@ -1,5 +0,0 @@
-package a00;
-
-public class Minus {
-
-}
---
+++ Programmers/level0/src/level0/몫.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class 몫 {
+	public static int solution(int num1, int num2) {
+		
+		int result = num1/num2;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(solution(7, 2));
+	}
+
+}
