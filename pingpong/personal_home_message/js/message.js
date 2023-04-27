bfc2c46281e080161698fbc5340a920c8eb6708a
JaeKyoung <worudeee@naver.com>
2023-04-27 21:22:52 +0900

    20230427_semi

---
+++ pingpong/personal_home_message/js/message.js
@@ -0,0 +1,7 @@
+const messagebuttonx = document.querySelector(".recieve-message-x");
+messagebuttonx.addEventListener("click", () => {
+    document.querySelector(".login-container").style.display = 'none';
+});
+
+const messageboxconfirm = document.querySelector(".")
+
