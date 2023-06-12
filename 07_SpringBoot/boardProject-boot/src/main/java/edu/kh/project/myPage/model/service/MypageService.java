1f40f0cc4d53bd09a5d9f02ebd4822b9dc01a404
jaekyoung <worudeee@naver.com>
2023-06-12 10:30:35 +0900

    20230612_인터셉터, 에러

---
+++ 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/myPage/model/service/MypageService.java
@@ -0,0 +1,46 @@
+package edu.kh.project.myPage.model.service;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import edu.kh.project.member.model.dto.Member;
+
+@Service // 비즈니스 로직 처리 클래스 + Bean 등록 (IOC)
+public interface MypageService {
+
+	/** 회원 정보 수정(?)
+	 * @param updateMember
+	 * @return
+	 */
+	int updateInfo(Member updateMember);
+
+	
+	/** 비밀번호 변경 서비스
+	 * @param currentPw
+	 * @param newPw
+	 * @param memberNo
+	 * @return result
+	 */
+	int changePw(String currentPw, String newPw, int memberNo);
+
+
+	/** 회원 탈퇴 서비스
+	 * @param memberPw
+	 * @param memberNo
+	 * @return result
+	 */
+	int secession(String memberPw, int memberNo);
+
+
+	/** 프로필 이미지 수정
+	 * @param profileImage
+	 * @param webPath
+	 * @param filePath
+	 * @param loginMember
+	 * @return result
+	 */
+	int updateProfile(MultipartFile profileImage, Member loginMember) throws IllegalStateException, IOException;
+
+}
