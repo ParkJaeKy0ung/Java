954b75c4d92a8a543cee460135884c2b73010d1e
jaekyoung <worudeee@naver.com>
2023-06-13 12:49:50 +0900

    20230613_게시판

--- 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/board/model/service/BoardService.java
+++ 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/board/model/service/BoardService.java
@@ -3,11 +3,39 @@ package edu.kh.project.board.model.service;
 import java.util.List;
 import java.util.Map;
 
+import edu.kh.project.board.model.dto.Board;
+
 public interface BoardService {
 
-	/** 게시판 종류 조회
+	/**
+	 * 게시판 종류 조회
+	 * 
 	 * @return boardTypeList
 	 */
 	List<Map<String, Object>> selectBoardTypeList();
+
+	// 게시글 목록 조회
+	Map<String, Object> selectBoardList(int boardCode, int cp);
+
+	// 게시글 상세 조회
+	Board selectBoard(Map<String, Object> map);
+
+	// 좋아요 여부 확인 서비스 호출
+	int boardLikeCheck(Map<String, Object> map);
+
+	// 좋아요 처리
+	int like(Map<String, Integer> paramMap);
+
+	// 조회 수 증가 서비스
+	int updateReadCount(int boardNo);
+
+	// 게시글 목록 조회 (검색)
+	Map<String, Object> selectBoardList(Map<String, Object> paramMap, int cp);
+
+	// 헤더 검색
+	List<Map<String, Object>> headerSearch(String query);
+
+	// DB 이미지(파일) 목록 조회
+	List<String> selectImageList();
 
 }
